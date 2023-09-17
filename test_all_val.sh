c3b75e2c37c791623285378f30a8096b4c087e4c
readmees <contact@meesmeester.nl>
2023-09-17 16:30:46 +0000

    Push before checkout

--- test_all_val.sh
+++ test_all_val.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(seq -f "%04g" 0 17)
+for i in $(seq -f "%05g" 0 86)
 do
-    bash test.sh data/test/${i}.png
+    bash test.sh roofdec/data/data_real/Residential/${i}.png
 done
